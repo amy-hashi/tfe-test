6615ee5d58d730b83d9e997bc701ef673eff7028
Amy Brown <amy@hashicorp.com>
2021-04-27 10:19:06 -0500

    back to modern tf code

--- main.tf
+++ main.tf
@@ -2,23 +2,23 @@ resource "random_id" "random" {
   #byte_length = 2097152
   byte_length = 10240 
   
-  #keepers = {
-  #  uuid = uuid()
-  #}
+  keepers = {
+    uuid = uuid()
+  }
 }
 
 output "random" {
-  value = "${random_id.random.hex}"
+  value = random_id.random.hex
 }
 
 resource "random_pet" "passwd" {
   length = 3
 
-  #keepers = {
-  #  uuid = uuid()
-  #}
+  keepers = {
+    uuid = uuid()
+  }
 }
 
 output "passwd" {
-  value = "${random_pet.passwd.id}"
+  value = random_pet.passwd.id
 }
